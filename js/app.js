3aa567f5c49f9abf58e45f4158b36ef06b8c43c2
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-09-15 20:18:33 -0700

    first pass at js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
-var luckyNumber;
-var guessCount;
-var guessPass;
-var newGuess;
-var bingo;
+var questionCount;
 
-$(document).ready(function(){
-	/*--- Create a new game ---*/
-	newGame();
-	
+$(document).ready(function(){	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
+  	$(".info").click(function(){
     	$(".overlay").fadeIn(1000);
   	});
 
@@ -18,106 +11,29 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	$("form").submit(function(e) {
-  		e.preventDefault();
+  	$('.circlebutton').mousedown(function() {
+		stateChange('dicaprio');	
+	});
+
+	$("form").submit(function(e) {
 		if (!bingo) {
 			newGuess = $('#userGuess').val();
-	  		$('#userGuess').val('');
-	  		guessPass = checkNumber(newGuess);
-	  		if(!guessPass) {
-	  			guessCount++;
-	  			updateCount(guessCount);
-	  			$("ul#guessList").append("<li>" +newGuess+ "</li>");
-	  			analyzeGuess(Math.abs(luckyNumber-newGuess));
-	  		}
+			$('#userGuess').val('');
+			guessPass = checkNumber(newGuess);
+	 		if(!guessPass) {
+	  			questionCount++;
+				updateCount(questionCount);
+			}
   		}
-  	});
-	
-	/*--- Click to start a new game ---*/
-	$(".new").click(function(e){
-  		e.preventDefault();
-  		newGame();
   	});
 });
 
-
-/*--- Start a new game ---*/
-function newGame () {
-	guessCount = 0;
-	bingo = false;
-	luckyNumber = getRandomInt(1,100);
-	updateCount(guessCount);
-	guessUpdate("Make your guess.");
-	$("ul#guessList li").remove();
-	$('#userGuess').val('');
+function stateChange(str) {
+    $('.actor').hide();
+    $('.' + str).show();
 }
 
-	/*--- Check if user guess meets requirements ---*/
-function checkNumber (newGuess) {
-	if (isNaN(newGuess)) {
-		guessUpdate("That's not a number!");
-		return true;
-	}
-	else if (newGuess < 1 || newGuess > 100) {
-		guessUpdate("Please give me a number between 1 and 100.");
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-/*--- Analyze user guess and provide feedback ---*/
-function analyzeGuess (x) {
-	if (x === 0) {
-		guessUpdate("Bingo!");
-		bingo = true;
-		buttonUpdate();
-	}
-	else if (x > 0 && x <= 10) {
-		guessUpdate("Burning up!");
-	}
-	else if (x > 10 && x <= 20) {
-		guessUpdate("Hot.");
-	}
-	else if (x > 20 && x <= 30) {
-		guessUpdate("Warm...");
-	}
-	else if (x > 30 && x <= 40) {
-		guessUpdate("Cold.");
-	}
-	else if (x > 40 && x <= 50) {
-		guessUpdate("Freezing!");
-	}
-	else {
-		guessUpdate("Sub-zero!");
-	}
-}
-
-/*--- Generate a random number ---*/
-function getRandomInt(min, max) {
-  	var randomInt = Math.floor(Math.random() * (max - min + 1) + min);
-  	return randomInt;
-}
-
-/*--- Update the guess count ---*/
+/*--- Update the question count ---*/
 function updateCount (count) {
-	$('#count').text(guessCount);
-}
-
-/*--- Change the temperature ---*/
-function guessUpdate (message) {
-	$('#feedback').text(message);
-}
-
-/*--- Change the button ---*/
-function buttonUpdate () {
-	$('#userGuess').hide();
-	$('#guessButton').on('click', function(e) {
-		e.preventDefault();
-		newGame();
-		$(this).val('Guess').off('click');
-		$('#userGuess').show();
-	})
-	.val('Start a new game!');
+	$('.currentquestion').text(questionCount);
 }
