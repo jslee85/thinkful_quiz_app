db5a3a8260f267c0d722e22479f2d4c19fd22cd1
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-09-21 20:08:29 -0700

    adding question array to javascript file

--- js/app.js
+++ js/app.js
@@ -1,4 +1,37 @@
-var questionCount = 0;
+var items = {
+	0: {
+		name: "clooney",
+		text: "George Clooney",
+		image: "../images/clooney.png",
+		correct: true
+	},
+	1: {
+		name: "dicaprio",
+		text: "Leonardo DiCaprio",
+		image: "../images/dicaprio.png",
+		correct: false
+	},
+	2: {
+		name: "williams",
+		text: "Michelle Williams",
+		image: "../images/williams.png",
+		correct: false
+	},
+	3: {
+		name: "smith",
+		text: "Will Smith",
+		image: "../images/smith.png",
+		correct: false
+	},
+	4: {
+		name: "portman",
+		text: "Natalie Portman",
+		image: "../images/portman.png",
+		correct: true
+	}
+};
+
+var currentItem = 0;
 var quizScore = 0;
 
 $(document).ready(function(){	
@@ -12,13 +45,13 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	/*--- Begin the question count ---*/
+  	/*--- Begin the question count 
 	$(".close").on('click', function() {
 		questionCount++;
 		updateCount(questionCount);
-  	});
+  	});---*/
 
-  	/*--- Hacky method ---*/
+	/*--- 
 	$(".circlebutton").on('click', function() {
 		stateChange('dicaprio');
 		questionCount++;
@@ -32,9 +65,28 @@ $(document).ready(function(){
 		else {
 			$('.one').addClass('incorrect');
 		}
-	});
+	});---*/
 });
 
+function loadData (itemNumber) {
+	$('.name').text(items[itemNumber].name);
+	$('.celebname').text(items[itemNumber].text);
+	$('.img').attr('src', items[itemNumber].image);
+}
+
+function checkAnswer (value) {
+	//check if user answer is correct
+	if (value == answers[currentItem].correct) {
+		//correct
+		quizScore+=20;
+		$('.one').addClass('correct');
+	}
+	else {
+		//incorrect
+		$('.one').addClass('incorrect');
+	}
+	loadData(itemNumber++);
+} 
 
 /*--- Show correct celebrity image ---*/
 function stateChange(str) {
@@ -51,3 +103,8 @@ function updateCount (count) {
 function updateScore (score) {
 	$('.points').text(quizScore);
 }
+
+/*--- Start a new game 
+function reloadPage() {
+	location.reload();
+}---*/
